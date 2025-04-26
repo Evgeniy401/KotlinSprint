d268614c5d6b0e25f2962939f249a25bed21f303
Admin <Evgeniy@>
2025-04-26 07:25:01 +0300

    KS-1-5 fifth task completed

--- src/main/kotlin/lesson1_task5.kt
+++ src/main/kotlin/lesson1_task5.kt
@@ -2,26 +2,15 @@ package org.example
 
 fun main() {
 
-    val length: Long = 40_868_600_000L
-    val age: Byte = 27
-    val partOfDay: Float = 0.075f
     val seconds: Short = 6480
-    val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: Int = 327000
 
-    println(length)
-    println(age)
-    println(partOfDay)
-    println(seconds)
-    println(partOfYear)
-    println(apogee)
+    val minutes = seconds / oneMinute
+    val remainingSeconds = seconds % oneMinute
+    val hours = minutes / oneMinute
+    val remainingMinutes = minutes % oneMinute
 
-    val minutes = seconds / 60
-    val remainingSeconds = seconds % 60
-    val hours = minutes / 60
-    val remainingMinutes = minutes % 60
-
-    println("0$hours:$remainingMinutes:0$remainingSeconds")
-
+    println(String.format("%02d:%02d:%02d", hours, remainingMinutes, remainingSeconds))
 
 }
+
+    const val oneMinute = 60
