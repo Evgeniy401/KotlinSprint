c2f5638ecd098e6a68c2478121eed1175d2ff6b2
Admin <Evgeniy@>
2025-04-27 07:10:06 +0300

    KS-2-1 first task completed

---
+++ src/main/kotlin/lesson_2/2_1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+    val grade1 = 3.0
+    val grade2 = 4.0
+    val grade3 = 3.0
+    val grade4 = 5.0
+    val numberOfGrade = 4
+
+    val gpa = (grade1 + grade2 + grade3 + grade4) / numberOfGrade
+    println(gpa)
+
+}
