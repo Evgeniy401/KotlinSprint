8b699f12e9550595d693cc894d45a3183d68f7ee
Admin <Evgeniy@>
2025-04-27 15:45:56 +0300

    KS-3-2 second task completed

---
+++ src/main/kotlin/lesson_3/3_2.kt
@@ -0,0 +1,17 @@
+package lesson_3
+
+fun main() {
+
+    var secondName = "Андреева"
+    val name = "Татьяна"
+    val surname = "Сергеевна"
+    var age = 20
+
+    println("$secondName $name $surname, $age")
+
+    age = 22
+    secondName = "Смирнова"
+
+    println("$secondName $name $surname, $age")
+
+}
