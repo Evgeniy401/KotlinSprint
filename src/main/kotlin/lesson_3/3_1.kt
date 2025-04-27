9e389926604900539658f0732277fa769a9205b7
Admin <Evgeniy@>
2025-04-27 15:33:15 +0300

    KS-3-1 first task completed

---
+++ src/main/kotlin/lesson_3/3_1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+
+    val userName = "Меткий Джо"
+    var greetings = "Приветствую тебя в этот великий день"
+    println("$greetings, $userName")
+
+    greetings = "Кажется, тебя ждет великая ночь"
+    println("$greetings, $userName")
+
+}
