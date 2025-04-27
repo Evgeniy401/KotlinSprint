020b519baa2e1371d1f03bdda0b529e2b010a38d
Admin <Evgeniy@>
2025-04-27 06:32:08 +0300

    KS-1-5 fifth task completed

--- src/main/kotlin/lesson1_task5.kt
+++ src/main/kotlin/lesson1_task5.kt
@@ -4,13 +4,13 @@ fun main() {
 
     val seconds: Short = 6480
 
-    val minutes = seconds / oneMinute
-    val remainingSeconds = seconds % oneMinute
-    val hours = minutes / oneMinute
-    val remainingMinutes = minutes % oneMinute
+    val minutes = seconds / ONE_MINUTE
+    val remainingSeconds = seconds % ONE_MINUTE
+    val hours = minutes / ONE_MINUTE
+    val remainingMinutes = minutes % ONE_MINUTE
 
     println(String.format("%02d:%02d:%02d", hours, remainingMinutes, remainingSeconds))
 
 }
 
-    const val oneMinute = 60
+const val ONE_MINUTE = 60
