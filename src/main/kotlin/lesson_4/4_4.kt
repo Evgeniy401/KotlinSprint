7b6371552870d81f9278a61d5204be194aa6e67a
Admin <Evgeniy@>
2025-04-30 08:48:48 +0300

    KS-4-4 fourth task completed

--- src/main/kotlin/lesson_4/4_4.kt
+++ src/main/kotlin/lesson_4/4_4.kt
@@ -3,17 +3,14 @@ package lesson_4
 fun main() {
 
     val trainingDay = 5
-    val armExercises: Boolean = (trainingDay % 2 != 0)
-    val abExercises: Boolean = (trainingDay % 2 != 0)
-    val legExercises: Boolean = (trainingDay % 2 == 0)
-    val backExercises: Boolean = (trainingDay % 2 == 0)
+    val isEven = trainingDay % 2 != 0
 
     println("""
         Сегодня нужно выполнить:
-        Упражнения для рук:    $armExercises
-        Упражнения для пресса: $abExercises
-        Упражнения для ног:    $legExercises
-        Упражнения для спины:  $backExercises
+        Упражнения для рук:    $isEven
+        Упражнения для пресса: $isEven
+        Упражнения для ног:    ${!isEven}
+        Упражнения для спины:  ${!isEven}
     """.trimIndent())
 
 }
