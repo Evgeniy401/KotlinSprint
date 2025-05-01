04ac3cbbc670403d584ed9307c1bba82daa3e774
Admin <Evgeniy@>
2025-05-01 08:54:08 +0300

    KS-4-3 third task completed

--- src/main/kotlin/lesson_4/4_3.kt
+++ src/main/kotlin/lesson_4/4_3.kt
@@ -2,13 +2,21 @@ package org.example.lesson_4
 
 fun main() {
 
-    val favorableConditions: Boolean = isSunny && isAwningOpen && airHumidity == 20 && timeOfYear != "зима"
+    val isSunny: Boolean = true
+    val isAwningOpen: Boolean = true
+    val airHumidity = 20
+    val isWinter = true
+
+    val favorableConditions: Boolean = (isSunny == isSunnyFavorableConditions) && (isAwningOpen == isAwningOpenFavorableConditions)
+        && (airHumidity == airHumidityFavorableConditions) && (isWinter == isWinterFavorableConditions)
 
     println("Благоприятные условия для роста бобовых: $favorableConditions")
 
 }
 
-const val isSunny: Boolean = true
-const val isAwningOpen: Boolean = true
-const val airHumidity = 20
-const val timeOfYear = "зима"
+const val isSunnyFavorableConditions: Boolean = true
+const val isAwningOpenFavorableConditions: Boolean = true
+const val airHumidityFavorableConditions = 20
+const val isWinterFavorableConditions: Boolean = false
+
+
