1383162dc294b3536b834b335b9408bd9783c5a2
Admin <Evgeniy@>
2025-05-02 11:05:17 +0300

    KS-5-4 fourth task completed

--- src/main/kotlin/lesson_5/5_4.kt
+++ src/main/kotlin/lesson_5/5_4.kt
@@ -5,16 +5,14 @@ fun main() {
     println("Введите ваше имя")
     val name = readln()
 
-    if (name == "Zaphod")
-
-    { println("Введите пароль")
+    if (name == "Zaphod") {
+        println("Введите пароль")
         val password = readln()
 
         if (password == "PanGalactic")
             println("Приветствуем Вас!")
-
         else println("Неверный пароль")
 
-    } else println("Пройти авторизацию")
+    } else {println("Пройти авторизацию")}
 
 }
