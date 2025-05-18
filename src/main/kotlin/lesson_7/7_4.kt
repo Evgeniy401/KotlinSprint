00f13536b0edd9d3ff564c6221d6a1da148f5336
Admin <Evgeniy@>
2025-05-18 17:51:22 +0300

    KS-7-4 (fourth task completed)

--- src/main/kotlin/lesson_7/7_4.kt
+++ src/main/kotlin/lesson_7/7_4.kt
@@ -5,12 +5,9 @@ fun main() {
     println("Введите время (в секундах):")
     val inputTime = readln().toInt()
 
-    for (i in inputTime downTo 0) {
-        if (i > 0) {
-            println("Осталось секунд: $i")
-        } else {
-            println("Время вышло")
-        }
+    for (i in inputTime downTo 1) {
+        println("Осталось секунд: $i")
         Thread.sleep(1000)
     }
+    println("Время вышло")
 }
