7c1e047926178b21d160b93f32ddaaa887b6f4b9
Admin <Evgeniy@>
2025-05-18 11:14:11 +0300

    KS-7-1 (first task completed)

--- src/main/kotlin/lesson_7/7_1.kt
+++ src/main/kotlin/lesson_7/7_1.kt
@@ -4,14 +4,14 @@ fun main() {
 
     var password = ""
 
-    var numbers = (0..9).random()
-    var symbol = ('a'..'z').random()
+    val chars: CharRange = 'a'..'z'
+    val ints: IntRange = 0..9
 
     for (i in 1..6) {
-        password += if (i % 2 == 1) {
-            symbol++.toString()
+        password += if (i % 2 == 0) {
+            chars.random()
         } else {
-            numbers++.toString()
+            ints.random()
         }
     }
 
