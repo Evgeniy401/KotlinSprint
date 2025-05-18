544467137dbc354772e23c671bb6d2f8664e750f
Admin <Evgeniy@>
2025-05-18 11:19:53 +0300

    KS-7-2 (second task completed)

--- src/main/kotlin/lesson_7/7_2.kt
+++ src/main/kotlin/lesson_7/7_2.kt
@@ -2,13 +2,14 @@ package org.example.lesson_7
 
 fun main() {
 
-    val code = (1000..9999).random()
+    var code: Int
+    var inputCode: Int
 
     do {
-        println("Ваш код авторизации: $code")
-        println("\nВведите код из СМС для авторизации:")
-
-        val inputCode = readln().toInt()
+        code = (1000..9999).random()
+        println("Ваш код авторизации N: $code")
+        println("Введите код")
+        inputCode = readln().toInt()
 
         if (inputCode == code) {
             println("Вы успешно авторизованы")
