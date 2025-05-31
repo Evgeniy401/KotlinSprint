b898f4a7f0476cfd9b1179553ba264e815fbb274
Admin <Evgeniy@>
2025-05-31 13:44:39 +0300

    KS-8-4 (fourth task completed)

--- src/main/kotlin/lesson_8/8_4.kt
+++ src/main/kotlin/lesson_8/8_4.kt
@@ -2,27 +2,22 @@ package org.example.lesson_8
 
 fun main() {
 
-    val ingredients =
-        arrayOf("картофель", "курица", "морковь", "лук", "грибы", "сметана", "чеснок", "сыр", "масло", "специи")
+    val ingredients = arrayOf("картофель", "курица", "морковь", "лук", "грибы", "сметана", "чеснок", "сыр", "масло", "специи")
 
-    for (i in ingredients) {
-        println(i)
-    }
+    println(ingredients.joinToString(", "))
 
-    println("\nВведите ингридиент, который вы хотите заменить:")
+    println("\nВведите ингредиент, который вы хотите заменить:")
     val inputIngredientChange = readln()
 
-    if (inputIngredientChange in ingredients) {
-        println("Какой ингридиент вы хотите добавить ?")
+    val index = ingredients.indexOf(inputIngredientChange)
+    if (index != -1) {
+        println("Какой ингредиент вы хотите добавить?")
         val inputIngredientAdd = readln()
-        ingredients[ingredients.indexOf(inputIngredientChange)] = inputIngredientAdd
+        ingredients[index] = inputIngredientAdd
 
         println("\nГотово! Вы сохранили следующий список:")
-        for (i in ingredients) {
-            println(i)
-        }
-
+        println(ingredients.joinToString(", "))
     } else {
-        println("Данного ингридиента нет в списке")
+        println("Данного ингредиента нет в списке")
     }
 }
