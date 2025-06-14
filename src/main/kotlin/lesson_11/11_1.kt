6955331c6e3e8ebdb8bc90935f04bfdb63218fd3
Admin <Evgeniy@>
2025-06-14 10:32:14 +0300

    KS-11-1 (first task completed)

--- src/main/kotlin/lesson_11/11_1.kt
+++ src/main/kotlin/lesson_11/11_1.kt
@@ -2,8 +2,8 @@ package org.example.lesson_11
 
 fun main() {
 
-    val vasily = User(1635131311,"vasily","vre2357f","vasily64626@gmail.com")
-    val peter = User(1635131388, "peter","arg&^46^%$*vg","peter7890098@gmail.com")
+    val vasily = User(1635131311, "vasily", "vre2357f", "vasily64626@gmail.com")
+    val peter = User(1635131388, "peter", "arg&^46^%$*vg", "peter7890098@gmail.com")
 
     println(vasily.id)
     println(vasily.login)
@@ -17,4 +17,4 @@ fun main() {
 
 }
 
-class User(val id: Int, val login: String, var password: String, var email: String) {}
+class User(val id: Int, val login: String, var password: String, var email: String)
