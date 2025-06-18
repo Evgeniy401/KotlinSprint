90162ce8913edfd70fa41b71ab2ee41f90a4e4a0
Admin <Evgeniy@>
2025-06-18 17:33:04 +0300

    KS-11-3 (third task completed)

--- src/main/kotlin/lesson_11/11_3.kt
+++ src/main/kotlin/lesson_11/11_3.kt
@@ -2,37 +2,32 @@ package org.example.lesson_11
 
 fun main() {
 
-    val fishing = Room("Красивая картинка", "Рыбалка", mutableListOf())
+    val fishing = Room(cover = "Красивая картинка", title = "Рыбалка", participants = mutableListOf())
 
     val randomMen = ParticipantRoom("Алеша")
     fishing.addParticipant(randomMen)
 
-    val participants = fishing.getParticipants()
-    println("Имя: ${participants[0].usersName}, Статус: ${participants[0].status}")
+    fishing.updateStatus("Алеша", UserStatus.TALKING)
+    println("${randomMen.usersName} - ${randomMen.status}")
 
 }
 
 class Room(
     val cover: String,
-    val roomName: String,
-    private val listParticipants: MutableList<ParticipantRoom> = mutableListOf(),
+    val title: String,
+    private val participants: MutableList<ParticipantRoom> = mutableListOf(),
 ) {
     fun addParticipant(newParticipant: ParticipantRoom) {
-        listParticipants.add(newParticipant)
+        participants.add(newParticipant)
     }
 
-    fun updateStatus(participant: MutableList<ParticipantRoom> = listParticipants) {
-        val currentStatus = participant[0].status
-        val newStatus = when (currentStatus) {
-            UserStatus.TALKING -> UserStatus.MICROPHONE_OFF
-            UserStatus.MICROPHONE_OFF -> UserStatus.MUTED
-            UserStatus.MUTED -> UserStatus.TALKING
+    fun updateStatus(usersName: String, newStatus: UserStatus) {
+        for (participant in participants) {
+            if (participant.usersName == usersName) {
+                participant.status = newStatus
+                return
+            }
         }
-        participant[0].status = newStatus
-    }
-
-    fun getParticipants(): List<ParticipantRoom> {
-        return listParticipants.toList()
     }
 }
 
