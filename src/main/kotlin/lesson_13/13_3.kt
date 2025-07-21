02037070d9b5671bf94da9ca4b032a8fe0fcdd73
Admin <Evgeniy@>
2025-07-21 07:02:17 +0300

    KS-13-3 (third task completed)

---
+++ src/main/kotlin/lesson_13/13_3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_13
+
+fun main() {
+
+    val contacts: MutableList<Subscriber3> = mutableListOf(
+        Subscriber3("Василий", 83285412958, "null"),
+        Subscriber3("Федор", 8391523895),
+        Subscriber3("Дмитрий", 8948456872, "ОАО Конфетка"),
+        Subscriber3("Александр", 8965896458, "ПАО Чита-дрита"),
+        Subscriber3("Даниил", 89209541257),
+    )
+
+    val companies: List<String> = contacts.mapNotNull { it.company }
+    print(companies)
+}
+
+class Subscriber3(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+)
