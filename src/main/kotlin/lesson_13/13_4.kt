e9f698525af94b8753cc1afadf50ed903034cbae
Admin <Evgeniy@>
2025-07-25 06:35:09 +0300

    KS-13-4 (fourth task completed)

--- src/main/kotlin/lesson_13/13_4.kt
+++ src/main/kotlin/lesson_13/13_4.kt
@@ -2,37 +2,33 @@ package org.example.lesson_13
 
 fun main() {
 
-    val contacts: MutableList<Subscriber4> = mutableListOf(
-        Subscriber4("Миша"),
-        Subscriber4("Василий", 83285412958, "null"),
-        Subscriber4("Федор", 8391523895),
-        Subscriber4("Дмитрий", 8948456872, "ОАО Конфетка"),
-        Subscriber4("Александр", 8965896458, "ПАО Чита-дрита"),
-        Subscriber4("Даниил", 89209541257)
-    )
+    val contacts: MutableList<Subscriber4> = mutableListOf()
+
+    println("Введите ваше имя:")
+    val name = readln()
+
+    println("Введите ваш номер телефона:")
+    val phoneNumber = readln().toLongOrNull()
+
+    println("Введите вашу компанию:")
+    val company = readln().takeIf { it.isNotBlank() }
+
+    if (phoneNumber == null) {
+        println("$name, вы не указали номер телефона")
+    } else {
+        val user1 = Subscriber4(name, phoneNumber, company)
+        contacts.add(user1)
+    }
 
     contacts.forEach { it.printData() }
-    println("\nPhone book: $phoneBook")
-
 }
-
-val phoneBook: MutableList<Long> = mutableListOf()
 
 class Subscriber4(
     private val name: String,
-    private val phoneNumber: Long? = null,
+    private val phoneNumber: Long,
     private val company: String? = null,
 ) {
     fun printData() {
-        return println("Имя: $name, Номер телефона: $phoneNumber, Компания: $company")
-    }
-
-
-    init {
-        if (phoneNumber == null) {
-            println("$name, вы не указали номер телефона")
-        } else {
-            phoneBook.add(phoneNumber)
-        }
+        println("Имя: $name, Номер телефона: $phoneNumber, Компания: ${company ?: "не указана"}")
     }
 }
